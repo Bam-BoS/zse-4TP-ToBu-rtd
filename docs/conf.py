3466ea4202bc5238b23d3d1a38056c0111fe054c
Bam_B0S <127388873+Bam-BoS@users.noreply.github.com>
2025-11-19 23:00:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
+project = 'Brain-Bad'
+copyright = '2024, Twoje Imię'
+author = 'Twoje Imię'
 
-import os
-import sys
-
-# -- Project information -----------------------------------------------------
-
-project = 'zse-docs-gh'
-copyright = '2025, 4TP'
-author = 'Tobiasz Burzynski'
-
-# -- General configuration ---------------------------------------------------
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-
+extensions = []
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+master_doc = 'index'
